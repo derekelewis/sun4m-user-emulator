08533f78f53443aab9b42ced19464caa4056fe7f
Derek Lewis <7467911+derekelewis@users.noreply.github.com>
2025-12-05 16:18:33 -0600

    parameterize steps to execute

--- sun4m/__main__.py
+++ sun4m/__main__.py
@@ -2,9 +2,14 @@ from .machine import Machine
 import argparse
 
 parser = argparse.ArgumentParser(description="SPARC V8 user-mode emulator")
-parser.add_argument("file", nargs="?", default="./bin/hello_world",
-                    help="ELF binary to execute (default: ./bin/hello_world)")
-parser.add_argument("--trace", action='store_true', help="enable tracing")
+parser.add_argument(
+    "file",
+    nargs="?",
+    default="./bin/hello_world",
+    help="ELF binary to execute (default: ./bin/hello_world)",
+)
+parser.add_argument("--steps", default=10000, help="number of cycles/steps to execute")
+parser.add_argument("--trace", action="store_true", help="enable tracing")
 args = parser.parse_args()
 
 
@@ -12,7 +17,7 @@ def main() -> None:
     machine: Machine = Machine(trace=args.trace)
     machine.load_file(args.file)
     # Run a bounded number of steps; the sample exits via syscall.
-    machine.cpu.run(max_steps=10_000)
+    machine.cpu.run(max_steps=args.steps)
 
 
 if __name__ == "__main__":
