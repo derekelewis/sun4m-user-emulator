decb3df66bea94a075fe7b72b46a5b50b37308b2
Derek Lewis <7467911+derekelewis@users.noreply.github.com>
2025-12-05 16:08:58 -0600

    parameterize file to execute

--- sun4m/__main__.py
+++ sun4m/__main__.py
@@ -1,14 +1,16 @@
 from .machine import Machine
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="SPARC V8 user-mode emulator")
+parser.add_argument("file", nargs="?", default="./bin/hello_world",
+                    help="ELF binary to execute (default: ./bin/hello_world)")
 parser.add_argument("--trace", action='store_true', help="enable tracing")
 args = parser.parse_args()
 
 
 def main() -> None:
     machine: Machine = Machine(trace=args.trace)
-    machine.load_file("./bin/hello_world")
+    machine.load_file(args.file)
     # Run a bounded number of steps; the sample exits via syscall.
     machine.cpu.run(max_steps=10_000)
 
