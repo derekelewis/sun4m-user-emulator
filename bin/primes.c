f78ff69d0a3cccbccb3929e236d880384ec02d98
Derek Lewis <7467911+derekelewis@users.noreply.github.com>
2025-12-05 14:47:02 -0600

    add primes.c

---
+++ bin/primes.c
@@ -0,0 +1,23 @@
+int calculate_primes(int n) {
+
+    int n_primes = 0;
+
+    for (int i = 2; i <= n; i++) {
+        int is_prime = 1;
+        for (int j = 2; j < i; j++) {
+            if ((i % j) == 0) {
+                is_prime = 0;
+                break;
+            }
+        }
+        if (is_prime)
+            n_primes++;
+    }
+
+    return n_primes;
+}
+
+int main() {
+    int n = calculate_primes(10000);
+    return 0;
+}
